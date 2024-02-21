72ff98193513650880e2e356c1254df3a9763a9e
Mr-Skribbls <woodchipschest@gmail.com>
2024-02-21 11:30:47 -0600

    Squashed commit of the following:
    commit 2aac644376d4f25724c066c01db2bd1ba97b857d
    Author: Mr-Skribbls <woodchipschest@gmail.com>
    Date:   Wed Feb 21 11:30:13 2024 -0600
    
        adds: phone to the contact page
    
    commit ef6252f1173760a8bfa68c6e2719a649d42e6c99
    Author: Mr-Skribbls <woodchipschest@gmail.com>
    Date:   Wed Feb 21 11:29:45 2024 -0600
    
        adds: phone and instagram to constants

--- src/pages/contact/contact.page.tsx
+++ src/pages/contact/contact.page.tsx
@@ -9,7 +9,8 @@ interface ContactProps {
 const Contact:FC<ContactProps> = () => {
   return (
     <div className="contact-page site-container">
-      <p>If you're interested in getting a puppy from us please send us an email or contact us on facebook.</p>
+      <p>If you're interested in getting a puppy from us please call, send us an email, or contact us on facebook.</p>
+      <p>phone: <a href={`tel:${constants.phoneNumber}`}>{constants.phoneNumberDisplay}</a></p>
       <p>email: <a href={`mailto:${constants.emailAddress}`}>{constants.emailAddress}</a></p>
       <p>facebook: <a rel='noreferrer' target='_blank' href={constants.facebookAddress}>{constants.companyName}</a></p>
     </div>
