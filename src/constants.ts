ef6252f1173760a8bfa68c6e2719a649d42e6c99
Mr-Skribbls <woodchipschest@gmail.com>
2024-02-21 11:29:45 -0600

    adds: phone and instagram to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
 const constants = {
   companyName: 'Positively Doodles',
   emailAddress: 'positivelydoodle@gmail.com',
-  phoneNumber: '',
+  phoneNumber: '1-618-816-5097',
+  phoneNumberDisplay: '1(618)816-5097',
   facebookAddress: 'https://www.facebook.com/profile.php?id=100077846943594',
   youtubeAddress: '',
-  instagramAddress: '',
+  instagramAddress: 'https://www.instagram.com/positivelydoodles/',
   pinterestAddress: '',
 };
 
