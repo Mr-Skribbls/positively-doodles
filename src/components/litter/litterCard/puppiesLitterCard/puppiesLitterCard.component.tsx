f1c55edab28ad4c1907659ccf28963a10d7ad344
Mr-Skribbls <woodchipschest@gmail.com>
2024-02-24 13:36:12 -0600

    Squashed commit of the following:
    commit 9393429cbf976a1f0af6ea915d85a9902375c089
    Author: Mr-Skribbls <woodchipschest@gmail.com>
    Date:   Sat Feb 24 13:31:39 2024 -0600
    
        improves: responsiveness of image and text for mobile devices
    
    commit f10e8a3b85436d4cdd33b58e1573b294f549398c
    Author: Mr-Skribbls <woodchipschest@gmail.com>
    Date:   Sat Feb 24 13:30:48 2024 -0600
    
        adds: puppy image modal when user click a puppy image
    
    commit 5997e9c47624263b4531d6976510c69052769dc9
    Author: Mr-Skribbls <woodchipschest@gmail.com>
    Date:   Sat Feb 24 13:26:15 2024 -0600
    
        updates: puppy status to include 'Available', 'Reserved', and 'Found Forever Home'

--- src/components/litter/litterCard/puppiesLitterCard/puppiesLitterCard.component.tsx
+++ src/components/litter/litterCard/puppiesLitterCard/puppiesLitterCard.component.tsx
@@ -1,22 +1,29 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import './puppiesLitterCard.css';
-import { LitterInfo, Puppy } from '../../../../dogInfo';
-import { isNil } from 'lodash';
+import { LitterInfo, Puppy, PuppyStatus } from '../../../../dogInfo';
+import { every, isNil } from 'lodash';
 import SourceSetImage from '../../../sourceSetImage/sourceSetImage.component';
 import useDateHelper from '../../../../hooks/useDateHelper';
+import Modal from '../../../modal/modal.component';
+import ModalGallery from '../../../modal/templates/modalGallery/modalGallery.component';
+import useImage, {ImageData} from '../../../../hooks/useImage';
 
 interface PuppiesLitterCardProps {
   litter: LitterInfo
 }
 
-const puppyCard = (puppy: Puppy, startingPrice: number, key: number) => {
+const puppyCard = (puppy: Puppy, startingPrice: number, openModalCB:(imageName: string) => void, key: number) => {
+  const statusClass = puppy.status === PuppyStatus.Reserved || puppy.status === PuppyStatus.Sold ? 'reserved' : 'available';
+  const price = puppy.status === PuppyStatus.Available ? `$${startingPrice + puppy.priceAboveStarting}` : puppy.status;
   return (
     <figure key={key} className='puppy-card'>
-      <SourceSetImage imageName={puppy.imageName} sizesRules={['250px']} />
+      <div className='image-wrapper' onClick={() => openModalCB(puppy.imageName)}>
+        <SourceSetImage imageName={puppy.imageName} sizesRules={['(max-width: 550px) 100vw','250px']} />
+      </div>
       <figcaption>
         <h4>{puppy.name}</h4>
-        <p className={puppy.available ? 'available' : 'reserved'}>{puppy.available ? `$${startingPrice + puppy.priceAboveStarting}` : "Reserved"}</p>
+        <p className={statusClass}>{price}</p>
         <p>{puppy.gender === "M" ? "Boy" : "Girl"}</p>
         <p>{puppy.description}</p>
       </figcaption>
@@ -26,12 +33,34 @@ const puppyCard = (puppy: Puppy, startingPrice: number, key: number) => {
 
 const PuppiesLitterCard:FC<PuppiesLitterCardProps> = ({litter}) => {
   const dateHelper = useDateHelper();
+  const {getImageDataByClassName, getImageDataByName} = useImage();
+  const [modalImageNames, setModalImageNames] = useState<string[] | null>();
+
+  const getModalImageNames = (baseImageData: ImageData): string[] => {
+    const relatedImages = getImageDataByClassName(baseImageData.classes, every);
+    return [baseImageData.name, ...relatedImages.map((imageData) => imageData.name)];
+  };
+
+  const openModal = (baseImageName: string): void => {
+    const baseImageData = getImageDataByName([baseImageName])[0];
+    setModalImageNames(getModalImageNames(baseImageData));
+  };
+
+  const closeModal = () => {
+    setModalImageNames(null);
+  };
+
   return (
-    <section className='puppies-litter-card'>
-      <h2>{litter.dam.name} and {litter.sire.name} Litter</h2>
-      {!isNil(litter.goHomeDate) && <span>Go home date: {dateHelper.hyphenatedDate(litter.goHomeDate)}</span>}
-      {!isNil(litter.puppies) && litter.puppies.map((puppy, key) => puppyCard(puppy, litter.startingPrice, key))}
-    </section>
+    <>
+      <section className='puppies-litter-card'>
+        <h2>{litter.dam.name} and {litter.sire.name} Litter</h2>
+        {!isNil(litter.goHomeDate) && <span>Go home date: {dateHelper.hyphenatedDate(litter.goHomeDate)}</span>}
+        {!isNil(litter.puppies) && litter.puppies.map((puppy, key) => puppyCard(puppy, litter.startingPrice, openModal, key))}
+      </section>
+      <Modal isOpen={!isNil(modalImageNames)} close={closeModal}>
+        {!isNil(modalImageNames) && <ModalGallery images={modalImageNames} />}
+      </Modal>
+    </>
   )
 };
 
