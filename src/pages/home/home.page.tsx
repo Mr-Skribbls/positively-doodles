23e82ae8bbbe333376da652418f4cc504655b114
Mr-Skribbls <woodchipschest@gmail.com>
2024-09-18 15:33:28 -0500

    adds baxter and bella page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -54,9 +54,9 @@ const Home:FC<HomeProps> = () => {
 
       <section className="badges">
         <div className="badge">
-          <a target='_blank' href="">
+          <Link to="baxter-and-bella">
             <SourceSetImage imageName='Baxter_and_Bella_Partners_Badge' sizesRules={['(max-width: 550px) 90%', '250px']} />
-          </a>
+          </Link>
         </div>
         <div className="badge">
           <a target='_blank' href="https://www.gooddog.com/breeders/positively-doodles-illinois">
