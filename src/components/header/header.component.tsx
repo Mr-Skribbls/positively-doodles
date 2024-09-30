960576705a50b1d5ebc7512f941a1ef06f1f644e
Brian Jensen <brian.jensen@earthsoft.com>
2024-09-30 14:05:42 -0500

    hides construction message adds google analytics tracker

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import './header.css';
 import Hero from '../hero/hero.component';
-import UnderConstruction from '../underConstruction/underConstruction.component';
+// import UnderConstruction from '../underConstruction/underConstruction.component';
 
 
 interface HeaderProps {
@@ -12,7 +12,7 @@ const Header:FC<HeaderProps> = () => {
   return (
     <header id="header">
       <Hero></Hero>
-      <UnderConstruction></UnderConstruction>
+      {/* <UnderConstruction></UnderConstruction> */}
     </header>
   );
 };
